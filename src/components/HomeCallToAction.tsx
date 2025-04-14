e069b8cc6bc780f9ca66a6d4740380b65314d4e1
DBjay01 <dhananjayvbile10@gmail.com>
2025-04-14 22:40:47 +0530

    First Feedback update

--- src/components/HomeCallToAction.tsx
+++ src/components/HomeCallToAction.tsx
@@ -11,7 +11,7 @@ export const HomeCallToAction = () => {
       <div className="container">
         <div className="section-heading relative items-center text-center">
           <h2 className="section-title text-2xl md:text-4xl font-bold">
-            Company Details
+          Our Location
           </h2>
                     
         </div>
@@ -21,37 +21,14 @@ export const HomeCallToAction = () => {
       {/* Company Details and Location */}
       <section className="py-8 md:py-16 bg-white">
         <div className="container mx-auto px-4">
-          <div className="md:flex items-start">
+          <div className="md:flex justify-center">
             {/* Company Details */}
-            <div className="md:w-1/2">
-              <h2 className="text-2xl md:text-3xl font-bold tracking-tighter text-black mb-4">
-                GurOm Dental
-              </h2>
-              <p className="text-sm md:text-lg text-gray-700 mb-2">
-                A403, Bhairavnath Complex, Pune Saswad Road, Opposite SP
-                Infocity, Bhekrai Nagar, Hadapsar, Pune, Maharashtra 412308,
-                India
-              </p>
-              <p className="text-sm md:text-lg text-gray-700 mb-2">
-                Contact No: +91 90904 14106
-              </p>
-              <p className="text-sm md:text-lg text-gray-700 mb-2">
-                Email:{" "}
-                <a
-                  href="mailto:sketchitup.official@gmail.com"
-                  className="text-blue-500"
-                >
-                  shopsdental@gmail.com
-                </a>
-              </p>
-            </div>
+            
 
             {/* Location Section */}
-            <div className="md:w-1/2 mt-8 md:mt-0 md:ml-8">
-              <h2 className="text-2xl md:text-3xl font-bold tracking-tighter text-black mb-4">
-                Our Location
-              </h2>
-              <div className="w-full md:w-[80%] h-64 rounded-md">
+
+            
+              <div className=" w-96 h-80 rounded-md ">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3783.8575171259367!2d73.94665347450604!3d18.490112070105244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2e9e2ea054583%3A0x27d1e1d4e804f634!2sBhairavnath%20Complex!5e0!3m2!1sen!2sin!4v1742299125969!5m2!1sen!2sin"
                   width="100%"
@@ -62,7 +39,7 @@ export const HomeCallToAction = () => {
                   aria-hidden="false"
                   tabIndex={0}
                 ></iframe>
-              </div>
+
             </div>
           </div>
         </div>
